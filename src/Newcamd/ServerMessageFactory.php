1efa18e2e922d3d68d29b270746d825a02041ec2
mszula <mateusz123321>
2016-03-16 23:54:56 +0100

    add ServerMessage\Crypt

--- src/Newcamd/ServerMessageFactory.php
+++ src/Newcamd/ServerMessageFactory.php
@@ -7,7 +7,10 @@ class ServerMessageFactory
     public static function create($message)
     {
         if (strlen($message) == 14) {
-            return new ServerMessage\Initial($message);
+            return (new ServerMessage\Initial(14))->set($message);
+        }
+        if ($message[0] == "\0" && ((strlen($message)-2) % 8) == 0) {
+            return (new ServerMessage\Crypt())->set($message);
         }
 
         return false;
