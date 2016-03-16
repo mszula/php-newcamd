b2293ad9c5f41605041fd79212dde23e420eec26
mszula <mateusz123321>
2016-03-16 23:57:01 +0100

    Add some tests

--- tests/ByteTest.php
+++ tests/ByteTest.php
@@ -18,9 +18,13 @@ class ByteTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->byte = new Byte(0);
-        $this->byte->set('Mateusz');
+        $this->byte->set('t')
+            ->setOne('e', 1)
+            ->prepend('Ma')
+            ->setOneAscii(117, 4)
+            ->append('sz');
     }
-    
+
     public function testGet()
     {
         $this->assertEquals("Mateusz", $this->byte->get());
@@ -38,5 +42,44 @@ class ByteTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(8, $this->byte->getLength());
     }
 
+    public function testGetOne()
+    {
+        $one = $this->byte->getOne(1);
+
+        $this->assertInstanceOf('Newcamd\Byte', $one);
+        $this->assertEquals('a', $one->get());
+    }
+
+    /*
+     * @depends testGetOne
+     */
+    public function testGetOneOrd()
+    {
+        $this->assertEquals(97, $this->byte->getOne(1)->ord());
+    }
+
+    /*
+     * @depends testGetOne
+     */
+    public function testGetOneHex()
+    {
+        $this->assertEquals(61, $this->byte->getOne(1)->hex());
+    }
+
+    /*
+     * @depends testGetOne
+     */
+    public function testGetOneDec()
+    {
+        $this->assertEquals(97, $this->byte->getOne(1)->dec());
+    }
+
+    public function testGetRange()
+    {
+        $one = $this->byte->getRange(2, 2);
+
+        $this->assertInstanceOf('Newcamd\Byte', $one);
+        $this->assertEquals('te', $one->get());
+    }
 
 }
