9a811993c65a37620ef9026fa018f4242b65a061
mszula <mateusz123321>
2016-03-27 23:03:06 +0200

    Add factory for DataLength, rename CardData to Card

--- src/Newcamd/ServerMessageFactory.php
+++ src/Newcamd/ServerMessageFactory.php
@@ -5,6 +5,10 @@ namespace Newcamd;
 class ServerMessageFactory
 {
 
+    /**
+     * @param string|Byte $message
+     * @return bool|ServerMessage\Response\CardData|ServerMessage\Response\DataLength|ServerMessage\Response\Initial|ServerMessage\Response\LoginSuccess
+     */
     public static function create($message)
     {
         if ($message instanceof Byte) {
@@ -13,14 +17,17 @@ class ServerMessageFactory
         if (strlen($message) == 14) {
             return (new ServerMessage\Response\Initial($message));
         }
+        if (strlen($message) == 2) {
+            return (new ServerMessage\Response\DataLength($message));
+        }
         if ($message[0] == ServerMessage\Response\LoginSuccess::MESSAGE_ID) {
             return (new ServerMessage\Response\LoginSuccess($message));
         }
-        if ($message[0] == ServerMessage\Response\CardData::MESSAGE_ID) {
-            return (new ServerMessage\Response\CardData($message));
+        if ($message[0] == ServerMessage\Response\Card::MESSAGE_ID) {
+            return (new ServerMessage\Response\Card($message));
         }
-        if (((strlen($message)-2) % 8) == 0) {
-            return (new ServerMessage\Crypt())->set(substr($message, 2));
+        if (($message % 8) == 0) {
+            return (new ServerMessage\Crypt())->set($message);
         }
 
         return false;
